cd1e95c84900e9e11918cda9fa5109a5a3436393
luca <luca@git.com>
2022-10-25 17:43:54 +0800

    second commit

--- src/main/java/com/atguigu/java/GitTest.java
+++ src/main/java/com/atguigu/java/GitTest.java
@@ -7,5 +7,6 @@ package com.atguigu.java;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git1");
+        System.out.println("hello Git2");
     }
 }
