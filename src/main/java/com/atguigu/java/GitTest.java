33eec6741f5742a65b520bafcd2e87dccdcadff2
luca <luca@git.com>
2022-10-25 17:42:18 +0800

    first commit

---
+++ src/main/java/com/atguigu/java/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.java;
+
+/**
+ * @author:HuangYongBing
+ * @time:2022-10-25 17:39:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git1");
+    }
+}
