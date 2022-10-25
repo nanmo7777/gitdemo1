a91ea347306045afd27d792f4220026c1ce4b11e
luca <luca@git.com>
2022-10-25 17:55:29 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/java/GitTest.java
+++ src/main/java/com/atguigu/java/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git1");
         System.out.println("hello Git2");
+        System.out.println("hello Git3");
+        System.out.println("hello Git4");
     }
 }
